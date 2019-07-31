11a88c0588359d5cb1b33f8f952ff1d742a457d6
Szilvia Kapuvári <kapuvari.szilvia@gmail.com>
2019-07-31 15:42:57 +0200

    első 5 feladat

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'angular-practice-001';
 
+  getName(): string {
+    return `Bond, James Bond`;
+  }
 }
+
