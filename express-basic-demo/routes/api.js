81075b89a18681d5dee65af3bcd918b4462d929a
Szilvia Kapuvári <kapuvari.szilvia@gmail.com>
2019-08-29 22:10:02 +0200

    express-basic-demo

---
+++ express-basic-demo/routes/api.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+const DB = require('../module/db');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.send('Please set api endpoint');
+});
+
+router.get('/:products/:id', (req, res, next) => {
+    let db = new DB(req.params.entity);
+    let id = req.params.id || 0;
+
+    db.find(id).then(
+        data => res.json(data),
+        err => res.json(err)
+
+    )
+})
+
+router.get('/:entity', (req, res, next) => {
+    let db = new DB(req.params.entity);
+
+    db.find().then(
+        data => res.json(data),
+        err => res.json(err)
+
+    )
+})
+module.exports = router;
