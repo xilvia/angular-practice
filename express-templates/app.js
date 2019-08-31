7b2d92830245bf51a01fb1cc3f5cae288b9566d9
Szilvia Kapuvári <kapuvari.szilvia@gmail.com>
2019-08-31 23:05:26 +0200

    custom template engine

---
+++ express-templates/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const port = 3999;
+
+const app = express();
+
+app.listen(port, () => {
+    console.log(`App listen in port: ${port}.`);
+});
+
+
+app.engine('ntl', require('./modules/ntl'));
+// ezen a ponton még akármilyen motor lehet
+app.set('views', './views') // specify the views directory
+// a nézethez a nyers template-fájlokat innen veszi
+app.set('view engine', 'ntl') // register the template engine
+// motor, mégpedig a megjelenítés motorja
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Home Page',
+        message: 'This is a homepage. Awesome!'
+    });
+});
